eb55f85bcccc1a2f6cf0d8dba5afd80c19935849
saisatyateja <saisatyateja18@gmail.com>
2026-08-08 01:34:18 +0530

    refactor(terraform): intoduce local variables and parameterized vpc networking

--- outputs.tf
+++
@@ -1,8 +0,0 @@
-output "aws_region" {
-  description = "Configured aws region"
-  value       = var.aws_region
-}
-output "environment" {
-  description = "Configured Environment"
-  value       = var.environment
-}
--- variables.tf
+++ variables.tf
@@ -8,3 +8,29 @@ variable "environment" {
   type        = string
   default     = "dev"
 }
+variable "vpc_cidr" {
+  description = "vpc cidr range"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+variable "public_subnet_1_cidr" {
+  description = "public subnet 1 cidr range"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+variable "public_subnet_2_cidr" {
+  description = "public subnet 2 cidr range"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "subnet_1_az" {
+  description = "subnet 1 availability zone"
+  type        = string
+  default     = "ap-south-2a"
+}
+variable "subnet_2_az" {
+  description = "subnet 2 availability zone"
+  type        = string
+  default     = "ap-south-2b"
+}
