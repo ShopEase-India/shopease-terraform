13d4838f66f1ab92c6d4e7f1c67b4e8c2181bd41
saisatyateja <saisatyateja18@gmail.com>
2026-08-08 00:28:17 +0530

    feat(vpc): associate route table with public subnet 2

--- main.tf
+++ main.tf
@@ -53,3 +53,8 @@ resource "aws_route_table_association" "public_1" {
   route_table_id = aws_route_table.public.id
   subnet_id      = aws_subnet.public_1.id
 }
+
+resource "aws_route_table_association" "public_2" {
+  route_table_id = aws_route_table.public.id
+  subnet_id = aws_subnet.public_2.id
+}
