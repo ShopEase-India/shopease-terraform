3fb8b272918b18a7135b5a8e62cb7585a596ef80
saisatyateja <saisatyateja18@gmail.com>
2026-08-07 22:47:43 +0530

    feat(terraform): add bootstrap outputs

---
+++ outputs.tf
@@ -0,0 +1,8 @@
+output "aws_region" {
+  description = "Configured aws region"
+  value       = var.aws_region
+}
+output "environment" {
+  description = "Configured Environment"
+  value       = var.environment
+}
