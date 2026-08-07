b1db4f895d9bbe79ea98847bcc57b7dabb114309
saisatyateja <saisatyateja18@gmail.com>
2026-08-07 23:53:37 +0530

    feat(vpc): create public subnet-1

--- main.tf
+++ main.tf
@@ -15,3 +15,13 @@ resource "aws_internet_gateway" "main" {
     Name = "shopease-dev-igw"
   }
 }
+
+resource "aws_subnet" "public_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-2a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "shopease-public-subnet-1"
+  }
+}
